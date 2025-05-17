be06b6fea3c9683e4a6134d75ae8309d1feebf06
jaablacketter <jaablacketter@gmail.com>
2025-05-17 10:36:35 -0500

    refactored main.py by moving count_words to seperate file

--- main.py
+++ main.py
@@ -6,26 +6,19 @@ def get_book_text(file_path):
         file_contents = file.read()
     return file_contents
 
-def count_words(text):
-    # Split the text into words
-    words = text.split()
-    
-    # Count the number of words
-    word_count = len(words)
-    
-    return word_count
-
 def main():
-   
+   # Import the count_words function from the stats module
+    from stats import count_words
+    
     # Define the path to the book file
     file_path = 'books/frankenstein.txt'
-    
+
     # Get the text from the book file
     file_contents = get_book_text(file_path)
 
     # Count the number of words in the book
     word_count = count_words(file_contents) 
-    
+
     # Print the word count
     print(f"{word_count} words found in the document")
 
---
+++ stats.py
@@ -0,0 +1,8 @@
+def count_words(text):
+    # Split the text into words
+    words = text.split()
+    
+    # Count the number of words
+    word_count = len(words)
+    
+    return word_count
